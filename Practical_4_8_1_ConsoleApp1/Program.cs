93608a86c0fe6c1b342e837bb4629a8b9e7afec1
User <genik-vlad@mail.ru>
2022-07-03 17:07:21 +0300

    Добавьте файлы проекта.

---
+++ Practical_4_8_1_ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practical_4_8_1_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue; // объявдение переменной минимума
+            Console.Write(" Введите длину последовательности :");
+            int length = int.Parse(Console.ReadLine()); // объявление длины массива
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(" Введите числа :");
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+            {
+                for (int i = 0; i < length; i++) //цикл проверки минимума
+                    if (array[i] < min)
+                    {
+                        min = array[i];
+                    }
+            }
+            Console.WriteLine(" Минимальное число в последовательности :{0}", min);
+            Console.ReadLine();
+        }
+    }
+}
